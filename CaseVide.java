a44d9540291a35fbd8f3a1c265c62356b4b53ae3
simbahhh <gilotsimon@gmail.com>
2017-12-12 19:27:06 +0100

    ajout de Plateau.toString()

--- CaseVide.java
+++ CaseVide.java
@@ -2,7 +2,6 @@
 
 public class CaseVide
 {
-
 	public CaseVide()
 	{
 		super();
--- Plateau.java
+++ Plateau.java
@@ -12,8 +12,14 @@ public class Plateau
 		ListeRobot = new Robot[2];
 	}
 	
-
-
-
-
+	public String toString(){
+		String s="";
+		for(int i=0; i<this.TabCases.length;i++){
+			for(int j=0;j<this.TabCases.length;j++){
+				s+=this.TabCases[i][j];
+			}
+			s+="\n";
+		}
+		return s;
+	}
 }
